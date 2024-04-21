b275096d46ea33abab792f1881a608229321304c
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-04-21 11:37:46 +0545

    notes index

---
+++ .history/tailwind.config_20240420164213.js
@@ -0,0 +1,33 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./vendor/wireui/wireui/resources/**/*.blade.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/View/**/*.php",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+        colors: {
+            test: colors.green,
+            primary: colors.indigo,
+            secondary: colors.gray,
+            positive: colors.emerald,
+            negative: colors.red,
+            warning: colors.amber,
+            info: colors.blue,
+        },
+    },
+    presets: [require("./vendor/wireui/wireui/tailwind.config.js")],
+    plugins: [forms],
+};
---
+++ .history/tailwind.config_20240420194927.js
@@ -0,0 +1,34 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+const colors = require('tailwindcss/colors')
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./vendor/wireui/wireui/resources/**/*.blade.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/View/**/*.php",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                test: #f97316,
+                test1: colors.white,
+                primary: colors.indigo,
+                secondary: colors.gray,
+                positive: colors.emerald,
+                negative: colors.red,
+                warning: colors.amber,
+                info: colors.blue,
+            },
+        },
+    },
+    presets: [require("./vendor/wireui/wireui/tailwind.config.js")],
+    plugins: [forms],
+};
